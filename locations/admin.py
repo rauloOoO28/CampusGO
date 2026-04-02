fb13998d7e3b9bc3afb201916c3e86af34eb8dd1
Oscar Pallares <osskr10@outlook.com>
2026-04-02 11:20:50 -0600

    Revert "Integración de apps core y locations con base de datos Postgres"
    This reverts commit 7be03c48f0aad5f51cbfdcbba7030e84fc636511.
    
    t

--- locations/admin.py
+++
@@ -1,9 +0,0 @@
-
-
-# Register your models here.
-from django.contrib import admin
-from .models import Escuela, Categoria, PuntoInteres
-
-admin.site.register(Escuela)
-admin.site.register(Categoria)
-admin.site.register(PuntoInteres)
